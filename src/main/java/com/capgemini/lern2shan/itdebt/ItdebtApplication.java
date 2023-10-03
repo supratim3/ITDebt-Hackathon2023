fe10e25019d5c6abfd661e1a6d7aec2ea99aeb1c
Supratim Dutta <supratim3@gmail.com>
2023-10-03 13:42:04 +0530

    initial version

---
+++ src/main/java/com/capgemini/lern2shan/itdebt/ItdebtApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.lern2shan.itdebt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItdebtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItdebtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/lern2shan/itdebt/ItdebtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.lern2shan.itdebt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItdebtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
